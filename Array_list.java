462571a6dc61e7364f6f146948b79e046c62355f
Dev <chovremahadev5@gmail.com>
2025-02-17 20:41:07 +0530

    second commit

--- Array_list.java
+++ Array_list.java
@@ -3,7 +3,7 @@ public class Array_list{
   public static void main(String[] args) {
       List<Object> al = new ArrayList<Object>();
       al.add(10);
-      al.add("VVIT");
+      al.add("VVIT DEV DEV");
       al.add(12.13);
       al.add(null);
       System.out.println(al);
